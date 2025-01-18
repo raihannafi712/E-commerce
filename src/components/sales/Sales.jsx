07429d3557359b2ed4f3a75796f70af2ea645130
raihannafi712 <shitabraihan712@gmail.com>
2025-01-19 01:33:56 +0600

    jeno

--- src/components/sales/Sales.jsx
+++ src/components/sales/Sales.jsx
@@ -10,8 +10,8 @@ import temp from '../../assets/tempFrame.png'
 
 const Sales = () => {
 
-    // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2025 00:00:00").getTime();
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2027 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
